7c7c7316e4a04658e51cad8a46aeb446e6747be9
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-10-17 16:29:13 -0300

    mensagem adicinada

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,3 +1,8 @@
+# texto1 recebe o valor "sou do" 
 texto1 = "sou do"
+
+# test02 recebe o valor "Back-And"
 texto2 = "Back-End"
+
+# a funsão print, concatena e imprimi as duas variaveis na tela
 print(texto1, texto2)
