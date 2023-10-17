650ef7a6814befc8ee801dd1cbe3247b35d207a2
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-10-17 16:43:13 -0300

    conteudo atualizado para ultilização de ESCAPE

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,8 +1,11 @@
+# voce pode escapara aspas duplas ultilizando aspas simples 
+# vale resaltar que isso so vale para aspas duplas, outras caractere não
 # texto1 recebe o valor "sou do" 
-texto1 = "sou do"
+texto1 = '"sou do"'
 
 # test02 recebe o valor "Back-And"
-texto2 = "Back-End"
+# caracter
+texto2 = "\"Back-End\""
 
 # a funsão print, concatena e imprimi as duas variaveis na tela
 print(texto1, texto2)
