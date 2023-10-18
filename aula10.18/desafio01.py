33962e269ea93b2e76e9212f2fc0d53521700b58
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-10-18 14:48:50 -0300

    desafio realizado

---
+++ aula10.18/desafio01.py
@@ -0,0 +1,7 @@
+# 1 crie um arquivo desafio01.py na pasta da aula de hoje
+# 2 criar três variaveis com nomes da sua escolha
+# 3 realize a subtração das tres variaveis e imprima o resultado na tela
+a = 10
+b = 20
+c = 30
+print(a + b + c)
