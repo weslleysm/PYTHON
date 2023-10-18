126464e538e85e8c285bffb89a61833974cb831e
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-10-18 16:20:17 -0300

    adicionado arquivo desafio02.py

---
+++ aula10.18/desafio02.py
@@ -0,0 +1,16 @@
+# 1 criar o arquivo desafio02.py
+# 2 pedir para o usuario informar sua idade
+# 3 pedir para o usuario informar a idade de um parente
+# 4 pedir para o usuario informar seu nome
+# 5 imprimir a multiplicação das duas idades
+# 6 imprimir o nome do usuario
+
+
+
+idade = input("digite sua idade: ")
+idade2 = input("digite a idade de algum parente: ")
+nome = input("digite seu nome: ")
+
+
+print (int (idade) * int (idade2))
+print(nome)
