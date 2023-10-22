81a08741fd22485d102e5489e05eb03217f51ef7
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-10-22 12:06:06 -0300

    arquivo desafio01.py criado

---
+++ desafio01.py
@@ -0,0 +1,10 @@
+# 1. Crie um código que peça a temperatura em graus Fahrenheit, transforme e imprima a temperatura em graus Celsius.
+#Formula: C = 5 * ((F-32) / 9)
+
+
+
+F = int (input("Informe a temperatura em graus Fahrenheit: "))
+
+C = int (5 * ((F-32) / 9))
+
+print("A temperatura em graus Celsius é:", C, "°C")
