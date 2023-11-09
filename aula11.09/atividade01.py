86aa2e8954f792dce09e5a7d493dfb8893b7889d
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-11-09 15:17:41 -0300

    pasta aula11.09 e atividade01.py criado

---
+++ aula11.09/atividade01.py
@@ -0,0 +1,19 @@
+#Texto da Atividade:
+
+#"Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que propor
+#ciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+# 1 Faça o fatiamento de 10 palavras do testo abaixo
+#  [i:f:p]
+
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+print(texto [0:5])
+print(texto[10:16])
+print(texto[20:30])
+print(texto [34:44])
+print(texto[49:53])
+print(texto[54:60])
+print(texto[61:66])
+print(texto [67:74])
+print(texto[76:88])
+print(texto[91:99])
