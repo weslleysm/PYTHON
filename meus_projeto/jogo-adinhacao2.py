8e3034917070a907cd69b767c1e7525e4c1c5401
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-11-14 13:38:18 -0300

    jogo de adivinhação modificado

--- meus_projeto/jogo-adinhacao2.py
+++ meus_projeto/jogo-adinhacao2.py
@@ -4,7 +4,21 @@ print(40*"*")
 print("Bem vindo ao jogo de Adivinhação!")
 print(40*"*")
 numero_secreto = random.randrange(1,101) # A FUNÇÃO random.randragem(inicio, fim) SERVE PARA GERAR UM NUMERO ALEATORIO
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 100
+
+print('Qual nivel de dificuldade?', numero_secreto)
+print('(1) Facil (2) Medio (3) Dificil')
+nivel = int(input('Defina o nivel: '))
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+
+
 
 
 for rodada in range (1, total_de_tentativas + 1):
@@ -23,14 +37,21 @@ for rodada in range (1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Parabéns! Você acertou!")
+        print("Parabéns! Você acertou e fez {} pontos".format(pontos))
         break
     else:
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
+            if rodada == total_de_tentativas:
+                print('O numero secreto era {} voce fez {}'.format(numero_secreto, pontos))
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
-
+            if rodada == total_de_tentativas:
+                print('O numero secreto era {}, voce fez {}'.format(numero_secreto, pontos))
+      
 
 
 print("Fim do jogo")
--- meus_projeto/jogo-adivinhacao.py
+++ meus_projeto/jogo-adivinhacao.py
@@ -4,8 +4,19 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 numero_secreto = random.randrange(1, 101) # a função random.randrage(inicio, fim) serve para gerar numero aleatorio
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
+pontos = 100
+
+print('Qual nivel de dificuldade?', numero_secreto)
+print('(1) Facil (2) Medio (3) Dificil')
+nivel = int(input('Defina o nivel: '))
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 
 while (rodada <= total_de_tentativas):
@@ -24,13 +35,20 @@ while (rodada <= total_de_tentativas):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Parabéns! Você acertou!")
+        print("Parabéns! Você acertou, sua pontuação é {}".format(pontos))
         break
     else:
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
+            if rodada == total_de_tentativas:
+                print('O numero secreto era {}, sua pontuação é {}'.format(numero_secreto, pontos))
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
+            if rodada == total_de_tentativas:
+                print('O numero secreto era {}, sua pontuação é {}'.format(numero_secreto, pontos))
 
     rodada = rodada + 1
 
