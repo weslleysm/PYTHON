bd673401ee5718b55d28b74ed923d838751467cc
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-11-18 08:18:25 -0300

    atividade 4 concuida

--- aula11.14/atividade4.py
+++ aula11.14/atividade4.py
@@ -1,18 +1,32 @@
 # 4-faca um codigo que pede 5 letras e quando for consoante ele avisa "É uma consoante" e imprima
-letra1 = input('informe a 1º letra')
-letra2 = input('informe a 2º letra')
-letra3 = input('informe a 3º letra')
-letra4 = input('informe a 4º letra')
-letra5 = input('informe a 5º letra')
+letra1 = input('informe a 1º letra:')
+letra2 = input('informe a 2º letra:')
+letra3 = input('informe a 3º letra:')
+letra4 = input('informe a 4º letra:')
+letra5 = input('informe a 5º letra:')
 
-lista_consoantes = []
-if letra1 != 'a' and letra1 != 'e' and letra1 != 'e' and letra1 != 'i' and letra1 != 'o':
+
+if letra1 != 'a' and letra1 != 'e' and letra1 != 'e' and letra1 != 'i' and letra1 != 'o' and letra1 != 'u':
     print('Letra 1 é consonate')
-if letra2 != 'a' and letra2 != 'e' and letra2 != 'e' and letra2 != 'i' and letra2 != 'o':
+else:
+    print('Letra 1 Não é consoante')
+
+if letra2 != 'a' and letra2 != 'e' and letra2 != 'e' and letra2 != 'i' and letra2 != 'o' and letra2 != 'u':
     print('Letra 2 é consonate')
-if letra3 != 'a' and letra3 != 'e' and letra3 != 'e' and letra3 != 'i' and letra3 != 'o':
+else:
+    print('Letra 2 Não é consoante')
+
+if letra3 != 'a' and letra3 != 'e' and letra3 != 'e' and letra3 != 'i' and letra3 != 'o' and letra3 != 'u':
     print('Letra 3 é consonate')
-if letra4 != 'a' and letra4 != 'e' and letra4 != 'e' and letra4 != 'i' and letra4 != 'o':
+else:
+    print('Letra 3 Não é consoante')
+
+if letra4 != 'a' and letra4 != 'e' and letra4 != 'e' and letra4 != 'i' and letra4 != 'o' and letra4 != 'u':
     print('Letra 4 é consonate')
-if letra5 != 'a' and letra5 != 'e' and letra5 != 'e' and letra5 != 'i' and letra5 != 'o':
+else:
+    print('Letra 4 Não é consoante')
+
+if letra5 != 'a' and letra5 != 'e' and letra5 != 'e' and letra5 != 'i' and letra5 != 'o' and letra5 != 'u':
     print('Letra 5 é consonate')
+else:
+    print('Letra 5 Não é consoante')
