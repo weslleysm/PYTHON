c273b2ee47ea7815fd9f6d92bba019922c759a71
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-11-20 14:24:25 -0300

    aula11.20 criado

---
+++ aula11.20/while.py
@@ -0,0 +1,8 @@
+contador = 0
+while contador < 9:
+# enquanto contador for menor que 9 faça
+    nota = float(input('informe uma nota'))
+    if nota < 0 or nota > 10:
+        break
+
+contador = contador + 1
