8646699d8cddde9eb5c6b70545711daa8d38971a
WESLLEY SEVERIANO <weslley.severiano16@gmail.com>
2023-11-23 09:56:43 -0300

    atv02 concluida

--- aula11.21/atv02.py
+++ aula11.21/atv02.py
@@ -1,3 +1,16 @@
-for n in range(0, 1001, 1):
-    if 
-        
+# Supondo que a população de um pais A sejaa de 80000 habitantes com uma taxa anual de crescimento de 3%,
+# Faça um programa que calcule e escreva o numero de anos necessarios para que a população do pais A chegue a 120000 habitantes
+
+inicial = 80000
+aumento_anual = (3 * inicial) / 100
+contador = 0
+valorfinal = inicial
+
+while True:
+    aumento = aumento_anual
+    valorfinal += aumento
+    contador += 1
+
+    if valorfinal >= 120000:
+        print(f'Há cada ano cresce {aumento_anual} habitantes, levando {contador} anos para atingir um valor de {valorfinal} habitantes')
+        break
